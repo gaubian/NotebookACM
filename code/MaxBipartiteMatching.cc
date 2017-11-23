176a73b5982a33258da667abe05b0ad84beb5e44
guimauve <guimauve@philibert.crans.org>
2017-11-23 17:40:28 +0100

    corrected prime and bipartite

--- code/MaxBipartiteMatching.cc
+++ code/MaxBipartiteMatching.cc
@@ -1,33 +1,21 @@
-// This code performs maximum bipartite matching.
-//
-// Running time: O(|E| |V|) -- often much faster in practice
-//
-//   INPUT: w[i] = edge between list of neighbours of node i
-//   OUTPUT: mr[i] = assignment for row node i, -1 if unassigned
-//           mc[j] = assignment for column node j, -1 if unassigned
-//           function returns number of matches made
 bool FindMatch(int i, const VVI &w, VI &mr, VI &mc, VI &seen) {
-  for(int j : w[i]) {
-    if (!seen[j]) {
-      seen[j] = true;
-      if (mc[j] < 0 || FindMatch(mc[j], w, mr, mc, seen)) {
-        mr[i] = j;
-        mc[j] = i;
-        return true;
-      }
+    for(int j : w[i]) if(!seen[j]) {
+        seen[j] = true;
+	if(mc[j] < 0 || FindMatch(mc[j], w, mr, mc, seen)) {
+            mr[i] = j;
+            mc[j] = i;
+            return true;
+	}
     }
-  }
-  return false;
+    return false;
 }
-
-int BipartiteMatching(const VVI &w, VI &mr, VI &mc) {
-  mr = VI(w.size(), -1);
-  mc = VI(w[0].size(), -1);
-  
-  int ct = 0;
-  for (int i = 0; i < w.size(); i++) {
-    VI seen(w[0].size());
-    if (FindMatch(i, w, mr, mc, seen)) ct++;
-  }
-  return ct;
+int BipartiteMatching(const VVI &w, VI &mr, VI &mc, int N, int M) {
+    mr = VI(N, -1);
+    mc = VI(M, -1);
+    int ct = 0;
+    for(int i = 0; i < N; i++) {
+        VI seen(M);
+        if (FindMatch(i, w, mr, mc, seen)) ct++;
+    }
+    return ct;
 }
--- code/Primes.cc
+++ code/Primes.cc
@@ -1,8 +1,11 @@
+using L = __uint128_t;
+
 L ipow (L a, L n, L M) {
     if (n == 0) return 1;
     L x = ipow((a * a) % M , n /2 , M) ;
     return n %2 ? ((a * x) % M) : x ;
 }
+
 bool strong_probable_prime (L n, L a) {
     if (n == a) return true ;
     int s = __builtin_ctz(n-1) ;
@@ -12,6 +15,7 @@ bool strong_probable_prime (L n, L a) {
 	if (ipow (a , d << r , n ) == n -1) return true ;
     return false ;
 }
+
 bool is_prime (L n) {
     return ( n == 2 || n % 2 != 0) && (n != 46856248255981) &&
     (strong_probable_prime (n,2)) && (strong_probable_prime (n,3)) &&
