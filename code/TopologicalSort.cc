c5843235cb9332062ee9a7c4c75fc16cebe96122
guimauve <guimauve@philibert.crans.org>
2017-11-24 14:23:23 +0100

    Fixed TopoSort

--- code/TopologicalSort.cc
+++ code/TopologicalSort.cc
@@ -8,21 +8,29 @@
 //
 // If no ordering is possible, false is returned.
 
+VVI rev(const VVI &w) {
+    int n = w.size();
+    VVI revw(n);
+    for(int i = 0; i < n; ++i)
+    for(int j : w[i]) revw[j].push_back(i);
+    return revw;
+}
+
 bool TopologicalSort (const VVI &w, VI &order){
+  VVI revw = rev(w);
   int n = w.size();
   VI parents (n);
   queue<int> q;
   order.clear();
   for (int i = 0; i < n; i++){
-    for (int j = 0; j < n; j++)
-      if (w[j][i]) parents[i]++;
-      if (parents[i] == 0) q.push (i);
+    for (int j : revw[i]) parents[i]++;
+    if (parents[i] == 0) q.push (i);
   }
   while (q.size() > 0){
     int i = q.front();
     q.pop();
     order.push_back (i);
-    for (int j = 0; j < n; j++) if (w[i][j]){
+    for (int j : w[i]) {
       parents[j]--;
       if (parents[j] == 0) q.push (j);
     }
