27ee7b080240e2081d5a69e867e99ab0309c110b
guimauve <guimauve@philibert.crans.org>
2017-11-24 14:39:54 +0100

    Fixed SCC

--- code/SCC.cc
+++ code/SCC.cc
@@ -1,37 +1,35 @@
-#include<memory.h>
-struct edge{int e, nxt;};
-int V, E;
-edge e[MAXE], er[MAXE];
-int sp[MAXV], spr[MAXV];
-int group_cnt, group_num[MAXV];
-bool v[MAXV];
-int stk[MAXV];
-void fill_forward(int x)
-{
-  int i;
-  v[x]=true;
-  for(i=sp[x];i;i=e[i].nxt) if(!v[e[i].e]) fill_forward(e[i].e);
-  stk[++stk[0]]=x;
-}
-void fill_backward(int x)
-{
-  int i;
-  v[x]=false;
-  group_num[x]=group_cnt;
-  for(i=spr[x];i;i=er[i].nxt) if(v[er[i].e]) fill_backward(er[i].e);
-}
-void add_edge(int v1, int v2) //add edge v1->v2
-{
-  e [++E].e=v2; e [E].nxt=sp [v1]; sp [v1]=E;
-  er[  E].e=v1; er[E].nxt=spr[v2]; spr[v2]=E;
-}
-void SCC()
-{
-  int i;
-  stk[0]=0;
-  memset(v, false, sizeof(v));
-  for(i=1;i<=V;i++) if(!v[i]) fill_forward(i);
-  group_cnt=0;
-  for(i=stk[0];i>=1;i--) if(v[stk[i]]){group_cnt++; fill_backward(stk[i]);}
+using graph = map<int,set<int>>;
+
+void Visit(int u, graph &G, stack<int> &st, set<int> &vis) {
+    if(vis.find(u) != vis.end()) return;
+    vis.insert(u);
+    for(int v : G[u]) Visit(v,G,st,vis);
+    st.push(u);
 }
 
+void Assign(graph &G, int u, int rt, map<int,int> &assi) {
+    if(assi.find(u) != assi.end()) return;
+    assi[u] = rt;
+    for(int v : G[u]) Assign(G,v,rt,assi);
+}
+
+graph rev(graph &G) {
+    graph Ans;
+    for(pair<int,set<int>> p : G)
+    for(int q : p.second) Ans[q].insert(p.first);
+    return Ans;
+}
+
+map<int,int> SCC(graph &G) {
+    set<int> vis;
+    stack<int> st;
+    map<int,int> assi;
+    for(pair<int,set<int>> p : G) Visit(p.first,G,st,vis);
+    graph GG = rev(G);
+    while(!st.empty()) {
+        int u = st.top();
+        st.pop();	
+        Assign(GG,u,u,assi);
+    }
+    return assi;
+}
