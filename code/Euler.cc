5e44f9ed1a0deb8005a8e9896ba0d81b6c812639
gaubian <guillaume.aubian@gaubian.xyz>
2020-01-24 18:18:58 +0100

    modified euler, segment tree and codesubj

--- code/Euler.cc
+++ code/Euler.cc
@@ -20,3 +20,29 @@ pair<bool,vector<int>> euler(vector<map<int,int>> grph) {
   if(!solved) return {false, ans};
   else return {true,ans};
 }
+
+// Eulerien recursif
+struct Arete
+{
+    int b,id;
+    Arete(int i,int j){b=i; id=j;}
+};
+const int NMAX=1001;
+const int MMAX=10001;
+vector<Arete> adjacence[NMAX];
+int idArete[NMAX];
+bool vuArete[MMAX];
+vector<int> cycle;
+
+
+void parcours(int noeud)
+{
+    while(idArete[noeud]<adjacence[noeud].size())
+    {
+        Arete& a=adjacence[noeud][idArete[noeud]++];
+        if(vuArete[a.id])continue;
+        vuArete[a.id]=true;
+        parcours(a.b);
+        cycle.push_back(noeud);
+    }
+}
--- code/SegmentTree.cc
+++ code/SegmentTree.cc
@@ -1,22 +1,31 @@
-// segment tree for minimum and sum
+const int INF = 1e9;
+
+// segment tree for minimum, maximum and sum
 // root is in tree[1], children in tree[2i] and tree[2i+1]
 // all ranges in build/updates/queries are 0-based
 struct Node {
-  int min, sum, up, size; // size of subtree
-  void update(int x) {
-    min += x;
-    sum += x*size;
-    up += x;
+  int min, max, sum, upA, upB, size; // size of subtree
+  void update(int a,int b) {
+    min = a*min+b;
+    max = a*max+b;
+    if(a<0)
+        swap(min,max);
+    sum = a*sum + b*size;
+    upA *= a;
+    upB = a*upB+b;
   }
   Node() {
     min = INF;
-    up = sum = size = 0;
+    max = -INF;
+    upB = sum = size = 0;
+    upA = 1;
   }
 };
 Node *tree;
 int N;
 void compute(int v) { // update tree[v] from its children, v < N
   tree[v].min = min(tree[2*v].min, tree[2*v+1].min);
+  tree[v].max = max(tree[2*v].max, tree[2*v+1].max);
   tree[v].sum = tree[2*v].sum + tree[2*v+1].sum;
 }
 // read values[0..size-1]
@@ -24,7 +33,7 @@ void build(int size, int values[]) {
   N = size > 1 ? 1 << ((int)log2(size-1)+1) : 1;
   tree = new Node[2*N];
   for(int i = 0; i < size; i++) { // leaves
-    tree[N+i].min = tree[N+i].sum = values[i];
+    tree[N+i].min = tree[N+i].max = tree[N+i].sum = values[i];
     tree[N+i].size = 1;
   }
   for(int i = N-1; i > 0; i--) { // interns
@@ -34,26 +43,32 @@ void build(int size, int values[]) {
 }
 void push(int v) {
   if(2*v < 2*N) // left subtree
-    tree[2*v].update(tree[v].up);
+    tree[2*v].update(tree[v].upA,tree[v].upB);
   if(2*v+1 < 2*N) // right subtree
-    tree[2*v+1].update(tree[v].up);
-  tree[v].up = 0;
+    tree[2*v+1].update(tree[v].upA,tree[v].upB);
+  tree[v].upA=1; 
+  tree[v].upB=0;
 }
+
+struct Infos
+{
+    int min,max,sum;
+};
 // v: current vertex with corressponding range [left, right)
-// find mimimum and sum of the range [l, r)
-pair<int, int> query_aux(int v, int left, int right, int l, int r) {
+// find minimum and sum of the range [l, r)
+Infos query_aux(int v, int left, int right, int l, int r) {
   push(v);
   if(right <= l || r <= left) // outside
-    return {INF, 0};
+    return {INF,-INF,0};
   if(l <= left && right <= r) // inside
-    return {tree[v].min, tree[v].sum};
+    return {tree[v].min, tree[v].max, tree[v].sum};
   int m = (left+right)/2;
-  int lm, rm, ls, rs;
-  tie(lm, ls) = query_aux(2*v, left, m, l, r); // left subtree
-  tie(rm, rs) = query_aux(2*v+1, m, right, l, r); // right
-  return {min(lm, rm), ls+rs};
+  Infos linfo = query_aux(2*v, left, m, l, r); // left subtree
+  Infos rinfo = query_aux(2*v+1, m, right, l, r); // right
+  return {min(linfo.min, rinfo.min), max(linfo.max, rinfo.max), linfo.sum+rinfo.sum};
 }
-pair<int, int> query(int l, int r) {
+
+Infos query(int l, int r) {
   return query_aux(1, 0, N, l, r);
 }
 // update element at index i with value x
@@ -74,20 +89,20 @@ void update(int i, int x) {
 }
 // v: the current vertex with corressponding range in [left, right)
 // add value x for element in range [l, r)
-void update_range_aux(int v, int left, int right, int l, int r, int x) {
+void update_range_aux(int v, int left, int right, int l, int r, int a, int b) {
   push(v);
   if(right <= l || r <= left)
     return;
   if(l <= left && right <= r) {
-    tree[v].update(x);
+    tree[v].update(a,b);
     return;
   }
   int m = (left+right)/2;
-  update_range_aux(2*v, left, m, l, r, x);
-  update_range_aux(2*v+1, m, right, l, r, x);
+  update_range_aux(2*v, left, m, l, r, a, b);
+  update_range_aux(2*v+1, m, right, l, r, a, b);
   compute(v);
 }
 // update [l,r)
-void update_range(int l, int r, int x) {
-  update_range_aux(1, 0, N, l, r, x);
+void update_range(int l, int r, int a, int b) {
+  update_range_aux(1, 0, N, l, r, a, b);
 }
